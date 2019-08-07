7a6cd9addaaa23d542e7fdf8fd469f0af25db2f1
Andy <andy.french@hotmail.co.uk>
2019-08-07 12:33:32 +0100

    Created  'getPost' method.

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PostService } from '../../services/post.service';
+import { Post } from '../../models/Post';
 
 @Component({
   selector: 'app-post',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
-
-  constructor() { }
+  post: Post;
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService
+  ) {}
 
   ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.postService.getPost(id).subscribe(post => this.post);
   }
-
 }
